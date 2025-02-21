e64e74ad0b1ef47ee6ed0037942b9f3876a99a77
ArvinYang1925 <arvinyang1925@gmail.com>
2025-02-21 22:30:00 +0800

    week6 環境建置

---
+++ week6/app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const pinoHttp = require("pino-http");
+
+const logger = require("./utils/logger")("App");
+const creditPackageRouter = require("./routes/creditPackage");
+const skillRouter = require("./routes/skill");
+const usersRouter = require("./routes/users");
+const adminRouter = require("./routes/admin");
+const coachesRouter = require("./routes/coaches");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(
+  pinoHttp({
+    logger,
+    serializers: {
+      req(req) {
+        req.body = req.raw.body;
+        return req;
+      },
+    },
+  })
+);
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/healthcheck", (req, res) => {
+  res.status(200);
+  res.send("OK");
+});
+app.use("/api/credit-package", creditPackageRouter);
+app.use("/api/coaches/skill", skillRouter);
+app.use("/api/users", usersRouter);
+app.use("/api/admin", adminRouter);
+app.use("/api/coaches", coachesRouter);
+
+// 修改：增加 JSON 解析錯誤的特殊處理
+// app.use((err, req, res, next) => {
+//   if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
+//     return res.status(400).json({
+//       status: 'failed',
+//       message: 'JSON 格式錯誤'
+//     });
+//   }
+//   next(err);
+// });
+
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  req.log.error(err);
+  res.status(500).json({
+    status: "error",
+    message: "伺服器錯誤",
+  });
+});
+
+module.exports = app;
